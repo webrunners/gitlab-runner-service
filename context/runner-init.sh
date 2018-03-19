dd6fa32f3714bee1e91697c878de31d693808266
Ruben Nicolaides <ruben.nicolaides@webrunners.de>
2018-03-19 23:25:06 +0100

    fix executor default

--- context/runner-init.sh
+++ context/runner-init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-set -a
-
-CONFIG_DIR=/etc/gitlab-runner
-OPTIONS=
-
-
 _get_registered_tokens(){
     local tokens
     tokens=$(grep token $CONFIG_DIR/config.toml | cut -d\" -f2)
@@ -27,20 +21,24 @@ unregister_all(){
 trap unregister_all SIGINT SIGTERM SIGHUP EXIT  # cannot be caught: SIGKILL SIGSTOP
 
 # Ensure sudo rights
-touch /etc/sudoers.d/gitlab-runner;
-echo '%gitlab-runner ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/gitlab-runner;
-chmod 0400 /etc/sudoers.d/gitlab-runner;
+touch /etc/sudoers.d/gitlab-runner
+echo '%gitlab-runner ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/gitlab-runner
+chmod 0400 /etc/sudoers.d/gitlab-runner
 
 # Ensure docker run permissions
 groupadd -g `stat -c"%g" /var/run/docker.sock` docker||true
 usermod -a -G docker gitlab-runner
 
-# You could use this in stack files environment tag:
+
+CONFIG_DIR=/etc/gitlab-runner
+
+# You could use this under stack files environment tag:
 #  - STACK={{index .Service.Labels "com.docker.stack.namespace"}}  # - STACK={{printf "%#v" .}}
-STACK=$(docker inspect `hostname` --format '{{index .Config.Labels "com.docker.stack.namespace"}}')
 if [[ ! $SERVICE ]]; then
+    #STACK=$(docker inspect `hostname` --format '{{index .Config.Labels "com.docker.stack.namespace"}}')
     SERVICE=$(docker inspect `hostname` --format '{{index .Config.Labels "com.docker.swarm.service.name"}}')
 fi
+
 DESCRIPTION=${SERVICE:?SERVICE var required}_`hostname`
 
 # Ensure config/secret is bound to myself
@@ -52,22 +50,21 @@ docker service update $SERVICE -d ${dCONFIG:+--config-rm $dCONFIG --config-add $
 . /var/run/secrets/$SERVICE || true
 . /runner || true
 
+export CI_SERVER_URL=${URL:-${CI_SERVER_URL:?CI_SERVER_URL required}}
+export CI_SERVER_TOKEN=${TOKEN:-${CI_SERVER_TOKEN:?CI_SERVER_TOKEN required}}
+export RUNNER_EXECUTOR=${EXECUTOR:-${RUNNER_EXECUTOR:-shell}}
+
 # Misc options
 if [[ $EXECUTOR == 'docker' ]]; then
     OPTIONS+=" --docker-image docker:latest --docker-volumes /var/run/docker.sock:/var/run/docker.sock"
 fi
 
 if [[ $TAG_LIST ]]; then
-    OPTIONS+=" --tag-list $TAG_LIST"
+    OPTIONS+=" --tag-list ${TAG_LIST// /,}"
 fi
 
-export DESCRIPTION EXECUTOR SERVICE STACK TAG_LIST
 
 # Main
-gitlab-runner register${OPTIONS} --executor $EXECUTOR -u $URL -r $TOKEN -n --description "$DESCRIPTION" --locked
+gitlab-runner register${OPTIONS} -n --description "$DESCRIPTION" --locked  # --executor ${EXECUTOR:-shell} -u ${URL:?URL required} -r ${TOKEN:?TOKEN required}
 
 /entrypoint $@
-
-
-
-
